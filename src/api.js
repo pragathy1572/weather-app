17721a2f462a27f2e2cc2662cb27038af031ff00
unknown <pragathy1572@gmail.com>
2024-06-15 18:37:40 +0530

    second commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'fe7aeae6f6msh9c2271d3b86abddp14526cjsnf9b8b779e198',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "57ade72de3f549b8c6cd16c7dc3d74a2"
